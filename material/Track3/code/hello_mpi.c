a94b9c818c8aecf0b22689932b3571bba5c41921
eshernan <eshernan@gmail.com>
2024-03-14 11:55:21 -0500

    Initial material for Track 3 MPI

---
+++ material/Track3/code/hello_mpi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "mpi.h"
+
+
+int main(int argc, char **argv)
+{
+  MPI_Init(&argc, &argv); // alt.: NULL,NULL
+  
+  int size, rank;
+
+  // Copy the communicator
+  MPI_Comm my_world = MPI_COMM_WORLD;
+
+  MPI_Comm_size(my_world, &size);
+  MPI_Comm_rank(my_world, &rank);
+
+  printf("I am rank %i out of %i!\n", rank, size);
+
+  MPI_Finalize();
+
+  return 0;
+}
